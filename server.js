e59447f2040c5d75db5eec272f7f545660c08e3f
Max Fathauer <maxfathauer@gmail.com>
2022-06-26 10:16:22 -0400

    added mongoosedb

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb:/localhost:27018/facenotes',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 mongoose.set('debug', true);
